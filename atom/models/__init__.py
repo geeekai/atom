0f7a661576d2d19a9e7d0dec93e25e4ec2e1eead
geeekai <alhamdoulilah540@gmail.com>
2025-03-14 10:19:19 +0100

    update

--- atom/models/__init__.py
+++ atom/models/__init__.py
@@ -1,3 +1,3 @@
-from .knowledge_graph import Entity, Relationship, KnowledgeGraph
+from .knowledge_graph import Entity, Relationship, KnowledgeGraph, RelationshipProperties
 
-__all__ = ["Entity", "Relationship", "KnowledgeGraph"]
+__all__ = ["Entity", "Relationship", "KnowledgeGraph", "RelationshipProperties"]
