1cae728415f935b1faa13c840ad7bf6f76a7dd47
geeekai <alhamdoulilah540@gmail.com>
2025-03-14 10:19:19 +0100

    update

--- atom/__init__.py
+++ atom/__init__.py
@@ -1,5 +1,4 @@
 from .documents_distiller import DocumentsDistiller
 from .graph_integration import GraphIntegrator
-from .itext2kg import iText2KG
-from .atom import AtomicText2KG
-__all__ = ['DocumentsDistiller', 'GraphIntegrator', 'AtomicText2KG', 'iText2KG']
+from .atom import Atom
+__all__ = ['DocumentsDistiller', 'GraphIntegrator', 'Atom']
